5be61b8af772ce5411f647471120460d2c9eaf5f
Jordan Vidrine <jordan@jordanvidrine.com>
2021-07-13 15:37:36 -0500

    Show category name abbreviations when no logo is present

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -12,3 +12,15 @@ registerUnbound("getCustomCategories", (settings) => {
 
   return categories;
 });
+
+registerUnbound("getAbbreviation", (categoryName) => {
+  let abbr = categoryName.replace(" and", "").split(" ")
+
+  if (abbr.length > 1) {
+    abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
+  } else {
+    abbr = abbr[0].charAt(0).toUpperCase() + abbr[0].charAt(1).toLowerCase();
+  }
+
+  return abbr;
+})
