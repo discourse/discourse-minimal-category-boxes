ca89c9ed2c8d961846b70bcd65945770e2543fc6
Jordan Vidrine <jordan@jordanvidrine.com>
2021-07-20 14:16:50 -0500

    Fix subcategory pages

--- javascripts/discourse/initializers/customize-categories-boxes.js
+++ javascripts/discourse/initializers/customize-categories-boxes.js
@@ -10,59 +10,13 @@ export default {
       api.modifyClass("component:categories-boxes", {
         tagName: "div",
         @discourseComputed()
-        firstCategories(categorySettings) {
-          let categories = [];
-          let categoriesSettings = categorySettings.split("|").map(id => Number(id))
-
-          categoriesSettings.forEach(categoryId => {
-            categories.push(Category.findById(categoryId))
-          })
-
-          return categories.length? categories : null;
-        },
-        @discourseComputed()
-        secondCategories() {
-          let secondCategories = [];
-          let secondCategoriesSettings = settings.second_categories.split("|").map(id => Number(id));
-
-          secondCategoriesSettings.forEach(categoryId => {
-            secondCategories.push(Category.findById(categoryId))
-          })
-
-          return secondCategories.length? secondCategories : null;
-        },
-        @discourseComputed()
-        thirdCategories() {
-          let thirdCategories = [];
-          let thirdCategoriesSettings = settings.third_categories.split("|").map(id => Number(id));
-
-          thirdCategoriesSettings.forEach(categoryId => {
-            thirdCategories.push(Category.findById(categoryId))
-          })
-
-          return thirdCategories.length? thirdCategories : null;
-        },
-        @discourseComputed()
-        fourthCategories() {
-          let fourthCategories = [];
-          let fourthCategoriesSettings = settings.fourth_categories.split("|").map(id => Number(id));
-
-          fourthCategoriesSettings.forEach(categoryId => {
-            fourthCategories.push(Category.findById(categoryId))
-          })
-
-          return fourthCategories.length? fourthCategories : null;
-        },
-        @discourseComputed()
-        fifthCategories() {
-          let fifthCategories = [];
-          let fifthCategoriesSettings = settings.fifth_categories.split("|").map(id => Number(id));
-
-          fifthCategoriesSettings.forEach(categoryId => {
-            fifthCategories.push(Category.findById(categoryId))
-          })
-
-          return fifthCategories.length? fifthCategories : null;
+        isCategoryPage() {
+          console.log(this.topicTrackingState.filterCategory);
+          if (this.topicTrackingState.filterCategory && this.topicTrackingState.filterCategory.has_children) {
+            return true;
+          } else {
+            return false;
+          }
         }
       })
     })
