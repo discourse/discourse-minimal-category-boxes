82aeebfff94494d7624b09f2ee3a3932ba03b43c
Jordan Vidrine <jordan@jordanvidrine.com>
2021-07-28 09:50:00 -0500

    REFACTOR

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -2,38 +2,6 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import Category from "discourse/models/category";
 
-registerUnbound("hasSettings", chosenCategories => {
-  if (chosenCategories.length === 0) {
-    return false;
-  } else {
-    return true;
-  }
-});
-
-registerUnbound("isIncluded", (chosenCategories, categoryId) => {
-  return (
-    chosenCategories
-      .split("|")
-      .map(id => Number(id))
-      .indexOf(categoryId) !== -1
-  );
-});
-
-registerUnbound("canViewChosenCategories", (chosenCategories, categories) => {
-  chosenCategories = chosenCategories.split("|").map(id => Number(id));
-  let availableCategories = categories.content.map(category => category.id);
-
-  availableCategories = availableCategories.filter(categoryId => {
-    return chosenCategories.indexOf(categoryId) !== -1;
-  });
-
-  if (availableCategories.length !== 0) {
-    return true;
-  } else {
-    return false;
-  }
-});
-
 registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
 
--- javascripts/discourse/initializers/customize-categories-boxes.js
+++ javascripts/discourse/initializers/customize-categories-boxes.js
@@ -9,6 +9,28 @@ export default {
     withPluginApi("0.8.14", api => {
       api.modifyClass("component:categories-boxes", {
         tagName: "div",
+        _allowedCategories(selectedCategories) {
+          let availableCategories = [];
+          
+          this.categories.forEach(category => {
+            if (availableCategories.indexOf(category.id === -1)) {
+              availableCategories.push(category.id);
+              if (category.has_children) {
+                availableCategories = [...availableCategories, ...category.subcategory_ids];
+              }
+            }
+          });
+
+          availableCategories = availableCategories.map(id => {
+            return Category.findById(id);
+          });
+
+          availableCategories = availableCategories.filter(category => {
+            return selectedCategories.indexOf(category.id) !== -1;
+          });
+
+          return availableCategories;
+        },
         @discourseComputed()
         isCategoryPage() {
           if (this.topicTrackingState.filterCategory && this.topicTrackingState.filterCategory.has_children) {
@@ -16,6 +38,26 @@ export default {
           } else {
             return false;
           }
+        },
+        @discourseComputed()
+        firstCategories() {
+          return this._allowedCategories(settings.first_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        secondCategories() {
+          return this._allowedCategories(settings.second_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        thirdCategories() {
+          return this._allowedCategories(settings.third_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        fourthCategories() {
+          return this._allowedCategories(settings.fourth_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        fifthCategories() {
+          return this._allowedCategories(settings.fifth_categories.split("|").map(id => Number(id)));
         },
       })
     })
