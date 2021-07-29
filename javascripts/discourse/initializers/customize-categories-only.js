0a6ad8d57210285ab15032f1ad611e5a11b8acae
Jordan Vidrine <jordan@jordanvidrine.com>
2021-07-29 11:23:52 -0500

    FIX: Add js for mobile component

---
+++ javascripts/discourse/initializers/customize-categories-only.js
@@ -0,0 +1,55 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import discourseComputed from "discourse-common/utils/decorators";
+import Category from "discourse/models/category";
+
+export default {
+  name: "customize-categories-boxes",
+
+  initialize() {
+    withPluginApi("0.8.14", api => {
+      api.modifyClass("component:categories-only", {
+        tagName: "div",
+        _allowedCategories(selectedCategories) {          
+          // filters categories to only include selected categories for each section
+          let availableCategories = this.site.categories.filter(category => {
+            if (selectedCategories.indexOf(category.id) !== -1) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+
+          return availableCategories;
+        },
+        @discourseComputed()
+        isCategoryPage() {
+          if (this.topicTrackingState.filterCategory && this.topicTrackingState.filterCategory.has_children) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        @discourseComputed()
+        firstCategories() {
+          return this._allowedCategories(settings.first_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        secondCategories() {
+          return this._allowedCategories(settings.second_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        thirdCategories() {
+          return this._allowedCategories(settings.third_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        fourthCategories() {
+          return this._allowedCategories(settings.fourth_categories.split("|").map(id => Number(id)));
+        },
+        @discourseComputed()
+        fifthCategories() {
+          return this._allowedCategories(settings.fifth_categories.split("|").map(id => Number(id)));
+        },
+      })
+    })
+  }
+}
