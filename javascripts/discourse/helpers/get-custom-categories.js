d2784a9875213095b1cf0fdc508702acc6053906
DK <69655652+syphrpunk@users.noreply.github.com>
2022-11-26 18:47:50 -0700

    Update get-custom-categories.js

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -6,7 +6,7 @@ registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
   abbr.replace("-","");
   if (abbr.length > 1) {
-    abbr = abbr[0].charAt(0).toLowerCase() + abbr[1].charAt(0).toLowerCase();
+    abbr = abbr[0].charAt(0).toLowerCase() + abbr[1].charAt(0).toLowerCase() + abbr[2].charAt(0).toLowerCase();
   } else {
     abbr = abbr[0].charAt(0).toLowerCase() + abbr[0].charAt(1).toLowerCase();
   }
