494d5372d75ceb084f5839389d2c8b4befd41863
DK <69655652+syphrpunk@users.noreply.github.com>
2022-11-26 18:58:16 -0700

    Update get-custom-categories.js

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -5,10 +5,6 @@ import Category from "discourse/models/category";
 registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
   
-  if (abbr[1].charAt(0) = "-") {
-    abbr = abbr.replace("-","");
-  }
-  
   if (abbr.length > 1) {
     abbr = abbr[0].charAt(0).toLowerCase() + abbr[1].charAt(0).toLowerCase();
   } else {
