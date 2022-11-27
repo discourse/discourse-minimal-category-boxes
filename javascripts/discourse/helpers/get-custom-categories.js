1fab56124cbed2b1861a212f06edf6c92842f505
DK <69655652+syphrpunk@users.noreply.github.com>
2022-11-26 18:57:32 -0700

    Update get-custom-categories.js

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -4,11 +4,16 @@ import Category from "discourse/models/category";
 
 registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
-  abbr.replace("-","");
+  
+  if (abbr[1].charAt(0) = "-") {
+    abbr = abbr.replace("-","");
+  }
+  
   if (abbr.length > 1) {
-    abbr = abbr[0].charAt(0).toLowerCase() + abbr[1].charAt(0).toLowerCase() + abbr[2].charAt(0).toLowerCase();
+    abbr = abbr[0].charAt(0).toLowerCase() + abbr[1].charAt(0).toLowerCase();
   } else {
     abbr = abbr[0].charAt(0).toLowerCase() + abbr[0].charAt(1).toLowerCase();
   }
+  
   return abbr;
 });
