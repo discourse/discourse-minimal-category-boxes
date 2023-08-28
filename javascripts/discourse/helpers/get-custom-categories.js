764ca2f170fcd9a3f82e1c6f5c72356ded7bc17d
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-08-28 14:18:25 +0800

    DEV: Removed unused lines

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -1,6 +1,4 @@
 import { registerUnbound } from "discourse-common/lib/helpers";
-import { withPluginApi } from "discourse/lib/plugin-api";
-import Category from "discourse/models/category";
 
 registerUnbound("getAbbreviation", categoryName => {
   let abbr = categoryName.replace(" and", "").split(" ");
