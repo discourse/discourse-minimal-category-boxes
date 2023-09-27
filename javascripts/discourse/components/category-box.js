93e8c6b33195334a422fdce811f0d0254d61d087
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-09-27 08:04:11 +0800

    DEV: Add theme CI workflow

--- javascripts/discourse/components/category-box.js
+++ javascripts/discourse/components/category-box.js
@@ -1,8 +1,13 @@
 import Component from "@glimmer/component";
+import { htmlSafe } from "@ember/template";
 
 export default class extends Component {
-  getAbbreviation(categoryName) {
-    let abbr = categoryName.replace(" and", "").split(" ");
+  get backgroundColor() {
+    return htmlSafe(`background-color: #${this.args.category.color}`);
+  }
+
+  get getAbbreviation() {
+    let abbr = this.args.category.name.replace(" and", "").split(" ");
 
     if (abbr.length > 1) {
       abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
